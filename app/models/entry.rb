ac8879c21e1ed9ad998f54cc6609f383eeb2d44c
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-23 12:07:40 -0400

    added charset method to entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,5 +2,12 @@ class Entry < ApplicationRecord
   belongs_to :user
   has_many :characters
   has_many :settings
+  has_many :character_character_settings, through: :characters, source: :character_settings
+  has_many :setting_character_settings, through: :settings, source: :character_settings
+
+  def character_settings
+    # (character_character_settings + setting_character_settings).uniq
+    CharacterSetting.find_by_sql("#{character_character_settings.to_sql} UNION #{setting_character_settings.to_sql}")
+  end
 
 end
--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -5,17 +5,5 @@ class EntrySerializer < ActiveModel::Serializer
   has_many :settings
 
 
-  def character_settings
-    self.object.settings.map do |setting|
-      setting.character_settings.map do |character_setting|
-        {id: character_setting.id,
-          character_id: character_setting.character_id,
-          setting_id: setting.id,
-          chapter: character_setting.chapter,
-          description: character_setting.description}
-        end
-    end
-  end
-
 
 end
