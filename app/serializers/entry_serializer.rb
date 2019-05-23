d18ba0b25dd683406fffe5d33dd120547b2cae25
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-23 13:02:12 -0400

    properly updated serializers to allow for access to character settings through entry

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,7 +1,8 @@
 class EntrySerializer < ActiveModel::Serializer
-  attributes :id, :genre, :title, :description, :content, :character_settings
+  attributes :id, :genre, :title, :description, :content
 
   has_many :characters
+  has_many :character_settings
   has_many :settings
 
 
