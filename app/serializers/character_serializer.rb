43ce400d374c963c65417f9f10676768e36cb56d
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-23 19:17:09 -0400

    got editing and deleting working for characters

--- app/serializers/character_serializer.rb
+++ app/serializers/character_serializer.rb
@@ -1,6 +1,10 @@
 class CharacterSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :entry_id
+  attributes :id, :name, :description, :entry
 
   has_many :character_settings
+
+  belongs_to :entry, serializer: CustomEntrySerializer
+
+
 
 end
