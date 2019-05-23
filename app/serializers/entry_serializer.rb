2861da921d818cb5be8ba114cf00e202421fa6ef
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-23 09:44:30 -0400

    added character setting to entry serializer

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,7 +1,21 @@
 class EntrySerializer < ActiveModel::Serializer
-  attributes :id, :genre, :title, :description, :content
+  attributes :id, :genre, :title, :description, :content, :character_settings
 
   has_many :characters
   has_many :settings
+
+
+  def character_settings
+    self.object.settings.map do |setting|
+      setting.character_settings.map do |character_setting|
+        {id: character_setting.id,
+          character_id: character_setting.character_id,
+          setting_id: setting.id,
+          chapter: character_setting.chapter,
+          description: character_setting.description}
+        end
+    end
+  end
+
 
 end
--- app/serializers/setting_serializer.rb
+++ app/serializers/setting_serializer.rb
@@ -1,6 +1,4 @@
 class SettingSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :entry_id
 
-  has_many :character_settings
-
 end
