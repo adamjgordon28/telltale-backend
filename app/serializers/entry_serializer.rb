bdd4d5a9a534060574728ed59dcf0ce8040fe002
adamjgordon28 <adamjgordon24@gmail.com>
2019-05-28 10:12:34 -0400

    styling

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,5 +1,5 @@
 class EntrySerializer < ActiveModel::Serializer
-  attributes :id, :genre, :title, :description, :content
+  attributes :id, :genre, :title, :description, :content, :user
 
   has_many :characters
   has_many :character_settings
