025655c7187e6ea4f829be82c21f2289868c742f
adamjgordon28 <adamjgordon24@gmail.com>
2019-07-05 18:27:33 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'topics#index'
   Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
@@ -12,6 +11,7 @@ Rails.application.routes.draw do
         post '/login', to: 'auth#create'
         get '/auto_login', to: 'auth#auto_login'
         get '/profile', to: 'users#profile',
+        root 'topics#index'
       end
     end
   end
