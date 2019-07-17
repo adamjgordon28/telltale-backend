4aa1cb97b6935c74ffb9dedc4c80ef14da908e4c
adamjgordon28 <adamjgordon24@gmail.com>
2019-07-17 16:10:23 -0400

    updated config/deploy.rb file to allow console

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,21 @@ set :deploy_to, "/home/deploy/telltale_backend"
 append :linked_files, "config/database.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
+namespace :rails do
+  desc 'Open a rails console `cap [staging] rails:console [server_index default: 0]`'
+  task :console do
+    server = roles(:app)[ARGV[2].to_i]
+
+    puts "Opening a console on: #{server.hostname}...."
+
+    cmd = "ssh #{server.user}@#{server.hostname} -t 'cd #{fetch(:deploy_to)}/current && RAILS_ENV=#{fetch(:rails_env)} bundle exec rails console'"
+
+    puts cmd
+
+    exec cmd
+  end
+end
+
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
